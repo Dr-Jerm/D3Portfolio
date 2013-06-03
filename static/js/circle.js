93b15809833ff4af86f2e823c50f651a7fe9ee8f
Jeremy <utilityjerm@gmail.com>
2013-06-02 22:54:21 -0600

    reverted code to get it in a runnable state

--- static/js/circle.js
+++ static/js/circle.js
@@ -1,6 +1,5 @@
 define(['require', 'exports', 'module'], function (require, exports, module) {
  
- // http://www.koalastothemax.com/
 	var Circle = function(svg, params){
 		this.radius = params.radius;
 
@@ -15,33 +14,10 @@ define(['require', 'exports', 'module'], function (require, exports, module) {
 				.style("fill", function(d, i) { return color(i % 3); });;
 		}
 
-		this.collide = function(){
-		var r = node.radius + 16,
-		nx1 = node.x - r,
-		nx2 = node.x + r,
-		ny1 = node.y - r,
-		ny2 = node.y + r;
-		return function(quad, x1, y1, x2, y2) {
-			if (quad.point && (quad.point !== node)) {
-				var x = node.x - quad.point.x,
-				y = node.y - quad.point.y,
-				l = Math.sqrt(x * x + y * y),
-				r = node.radius + quad.point.radius;
-				if (l < r) {
-				l = (l - r) / l * .5;
-				node.x -= x *= l;
-				node.y -= y *= l;
-				quad.point.x += x;
-				quad.point.y += y;
-				}
-			}
-		return x1 > nx2
-			|| x2 < nx1
-			|| y1 > ny2
-			|| y2 < ny1;
-		};
-	}
+		this.testTest = function(){
+			console.log(this.test);
 		}
+		// console.log("Exported Circle!");
 	}
 
 	module.exports = Circle;
